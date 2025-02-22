bc45a5d080e58748be6a806edc68aa6f78ceb071
mohamed-ouanjine-codecademy <email.for.tv.elhajji@gmail.com>
2025-02-22 18:11:05 +0100

    API_BASE change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://trader-vote-app.onrender.com';
 
 export const fetchTraders = async () => {
   const response = await axios.get(`${API_BASE}/traders`);
